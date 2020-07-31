df7db9246a51f8d0b5cd7fc1ed3f66af00d4aad9
Laura Malo <44496034+laumalo@users.noreply.github.com>
2020-07-31 16:06:49 +0200

    Update test_rotamers.py

--- benchmarks/rotamers/tests/test_rotamers.py
+++ benchmarks/rotamers/tests/test_rotamers.py
@@ -3,8 +3,8 @@ This module contains the test to check the rotamers library.
 """
 
 class TestRotamers(mae_file, pdb_file):
-	def test_groups(self):
-		return
+	#def test_groups(self):
+	#	return
 
 	def test_bonds(self):
 		bonds_offpele = get_bonds(pdb_file)
@@ -12,5 +12,5 @@ class TestRotamers(mae_file, pdb_file):
 		diff1, diff2 = differentiate(bonds_offpele,bonds_PlopRotTemp)
 
 
-	def test_resolution(self):
-		return
+	#def test_resolution(self):
+	#	return
