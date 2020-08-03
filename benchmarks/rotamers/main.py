25c6f0004b75e757f1c15442b399000d0a6f7ca4
Laura Malo <44496034+laumalo@users.noreply.github.com>
2020-08-03 16:58:56 +0200

    Updated files

--- benchmarks/rotamers/main.py
+++ benchmarks/rotamers/main.py
@@ -4,7 +4,9 @@ This module is designed in order to run the different test for the rotamers libr
 PlopRotTemp and offpele. 
 """
 import argparse
-from .test_rotamers import TestRotamers
+from test_rotamers import TestRotamers
+import utils
+import os.path as os
 
 
 def parse_args():
@@ -15,34 +17,45 @@ def parse_args():
     parser = argparse.ArgumentParser(description="Compute Statistics for the merged files")
     parser.add_argument("rotamer_schrodinguer", type=str, help="File obtained by PELE with Schodinguer")
     parser.add_argument("rotamer_offpele", type=str, help="File obtained by PELE with offpele")
-    parser.add_argument("-test" "--test_type", type=str, default="bonds", help= "Type of test to perform with the rotamers libreries")
     parser.add_argument("-o", "--output_path", type=str, default="", help="Path where to write the results")
     parser.add_argument("-of", "--output_file", type=str, default="diff.csv", help="File with differences in the two files, if there are")
+    parser.add_argument("-t","--test_type", metavar="NAME", type=str, default="resolution", help= "Type of test to perform with the rotamers libreries")
     args = parser.parse_args()
-    return args
+    return args.rotamer_schrodinguer,args.rotamer_offpele, args.output_path, args.output_file, args.test_type
 
 
-def perform_test(test_type, mae_input_file, pdb_input_file)
-    rotamers = TestRotamers(mae_input_file, pdb_input_file)
-    if test_type == bonds: 
+def perform_test(file,test_type, mae_input_file, pdb_input_file):
+    """
+    Creates the object rotamers with the rotameres library obtained with offpele and with PlopRotTemp and performs the
+    selected test on the data.
+    """
+    rotamers = TestRotamers(mae_input_file, pdb_input_file, file)
+    if test_type == 'bonds': 
         rotamers.test_bonds()
 
-    if test_type == groups: 
+    if test_type == 'groups': 
         rotamers.test_groups()
 
-    if test_type == resolution: 
+    if test_type == 'resolution': 
         rotamers.test_resolution()
 
+    if test_type == 'all': 
+        rotamers.test_bonds()
+        rotamers.test_resolution()
+        rotamers.test_groups()
 
-def main(mae_input_file, pdb_input_file, test_type,output_path, output_file):
+
+def main(mae_input_file, pdb_input_file, output_path, output_file, test_type):
 
     args = parse_args()
     if output_path != "": output_file = os.join(output_path,output_file)
     f = open(output_file, 'w')
-    perform_test(test_type,mae_input_file, pdb_input_file)
+    mae_file = open(mae_input_file,'r')
+    pdb_file = open(pdb_input_file, 'r')
+    perform_test(f, test_type,mae_file, pdb_file)
     f.close()
 
 
 if __name__ == "__main__":
-    mae_input_file, pdb_input_file, test_type, out_path, out_file = parse_args()
-    main(mae_input_file, pdb_input_file,test_type, out_path, out_file)
+    mae_input_file, pdb_input_file, out_path, out_file, test_file = parse_args()
+    main(mae_input_file, pdb_input_file, out_path, out_file, test_file)
