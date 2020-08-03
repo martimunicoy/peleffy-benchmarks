e263a6baf17ada5eb9df8924d35bafedd4501fb2
Laura Malo <44496034+laumalo@users.noreply.github.com>
2020-08-03 16:59:33 +0200

    Updated files

--- benchmarks/rotamers/run_example.sh
+++ benchmarks/rotamers/run_example.sh
@@ -1,9 +1,9 @@
 
 
 
-python /home/lauramalo/repos/offpele/offpele/main.py /home/lauramalo/repos/offpele-benchmarks/benchmarks/Plop_ligands/pdb/[file_name.pdb] -o RIG.rot.temp /home/lauramalo/tests/results/pdb/
+python /home/lauramalo/repos/offpele/offpele/main.py /home/lauramalo/repos/offpele-benchmarks/benchmarks/Plop_ligands/pdb/3.pdb -o RIG.rot.temp /home/lauramalo/tests/results/pdb/
 
-PlopRotTemp /home/lauramalo/repos/offpele-benchmarks/benchmarks/Plop_ligands/mae/[file_name.mae]  --rotamerdir /home/lauramalo/tests/results/mae/
+PlopRotTemp /home/lauramalo/repos/offpele-benchmarks/benchmarks/Plop_ligands/mae/3.mae  --rotamerdir /home/lauramalo/tests/results/mae/
 
 python /home/lauramalo/repos/offpele-benchmarks/benchmarks/rotamers/main.py /home/lauramalo/tests/results/pdb/LIG.rot.assign /home/lauramalo/tests/results/mae/LIG.rot.assign -of /home/lauramalo/tests/results/diff/
 
