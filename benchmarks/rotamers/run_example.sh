64670cce92a0549a8e179bdad30ffb02db2621ec
Laura Malo <44496034+laumalo@users.noreply.github.com>
2020-08-05 09:49:49 +0200

    Updated files

--- benchmarks/rotamers/run_example.sh
+++ benchmarks/rotamers/run_example.sh
@@ -1,11 +1,11 @@
 
 
 
-python /home/lauramalo/repos/offpele/offpele/main.py /home/lauramalo/repos/offpele-benchmarks/benchmarks/Plop_ligands/pdb/3.pdb -o RIG.rot.temp /home/lauramalo/tests/results/pdb/
+python /home/lauramalo/repos/offpele/offpele/main.py /home/lauramalo/repos/offpele-benchmarks/benchmarks/Plop_ligands/pdb/3.pdb -o /home/lauramalo/tests/results/pdb/
 
 PlopRotTemp /home/lauramalo/repos/offpele-benchmarks/benchmarks/Plop_ligands/mae/3.mae  --rotamerdir /home/lauramalo/tests/results/mae/
 
-python /home/lauramalo/repos/offpele-benchmarks/benchmarks/rotamers/main.py /home/lauramalo/tests/results/pdb/LIG.rot.assign /home/lauramalo/tests/results/mae/LIG.rot.assign -of /home/lauramalo/tests/results/diff/
+python /home/lauramalo/repos/offpele-benchmarks/benchmarks/rotamers/main.py  /home/lauramalo/tests/results/mae/LIG.rot.assign /home/lauramalo/tests/results/pdb/LIG.rot.assign -of /home/lauramalo/tests/results/diff/
 
 rm /home/lauramalo/tests/results/pdb/LIG.rot.assign
 
