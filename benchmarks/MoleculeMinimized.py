a15a2b5636e04cb3a70e921d80878461793a763f
Laura Malo <44496034+laumalo@users.noreply.github.com>
2020-08-21 12:18:12 +0200

    Add files via upload

---
+++ benchmarks/MoleculeMinimized.py
@@ -0,0 +1,74 @@
+import os 
+
+class MoleculeMinimized:
+	"""
+	It contains all the tools to minimize a molecule with the OpenForceField toolkit for PELE.
+	"""
+
+	def __init__(self, input_file, PELE_version):
+		"""
+		It initializes a MocelueMinimized object. 
+
+		Parameters: 
+		----------
+		input_file: PDB with the parameters of the molecule. 
+
+		PELE_version: str
+					  path of an executable version of PELE
+
+
+		Examples: 
+		----------
+
+		Load a molecule from a PDB file and minimize it in the vacuum and OBC solvent. 
+        >>> import MoleculeMinimized as MM
+
+		>>> new_molecule = MM.MoleculeMinimized('ligand.pdb', '/home/municoy/builds/PELE/PELE-repo_serial/PELE-1.6')
+		>>> new_molecule.minimize(input_file = 'ligand.pdb', PELE_version = '/home/municoy/builds/PELE/PELE-repo_serial/PELE-1.6')
+
+		"""
+		self.input_file = input_file
+		self.PELE_version = PELE_version
+
+	def _output_folder(self,input_file):
+		"""
+		It creates an output folder with a copy of the ligand's pdb where all the results will be saved. 
+		"""
+		os.system("mkdir output")
+		os.system("cp %s ./output/" % (input_file))
+		os.system("cd output")
+		os.system("mv ./output/%s ./output/ligand.pdb" % (input_file))
+		os.system("cd output")
+
+
+	def _generate_parameters(self):
+		"""
+		It generates the parameters of the molecule (from the input_file) as DataLocal in the output folder.
+		"""
+		os.system("python /home/lauramalo/repos/offpele/offpele/main.py ./output/ligand.pdb --with_solvent --as_DataLocal")
+		os.system("mv ./DataLocal/ ./output/DataLocal/")
+
+	def _link_folders(self):
+		"""
+		It links the encessary folders to the output folder.
+		"""
+		os.system("ln -s /home/municoy/repos/PELE-repo/Data ./output/")
+		os.system("ln -s /home/municoy/repos/PELE-repo/Documents ./output/") 
+
+	def minimize(self,input_file, PELE_version):
+		"""
+		It minimized the molecule with the open forcefield (OFFPELE). 
+
+		Parameters:
+		----------
+		input_file: .pdb with the parameters of the molecule. 
+
+		PELE_version: str
+					  path of an executable version of PELE
+
+		"""
+		self._output_folder(input_file)
+		self._link_folders()
+		self._generate_parameters()
+		os.system(" %s /home/lauramalo/tests/geometry/VACUUM_minimization.conf > ./output/VACUUM_minimization.out" % (PELE_version))
+		os.system(" %s /home/lauramalo/tests/geometry/OBC_minimization.conf > ./output/OBC_minimization.out" % (PELE_version))
---
+++ benchmarks/hydrationfreeenergy.py
@@ -0,0 +1,56 @@
+"""
+It computes the Hydration Free Energy for a molecule from a PDB file. 
+
+Example: 
+---------
+
+>>>  python hydrationfreeenergy.py ligand.pdb 
+"""
+
+import argparse
+import os.path 
+import re
+import os 
+import MoleculeMinimized as MM
+import DiffEnergies as DiffEnergies
+
+
+def parse_args():
+    """
+        Parse command line arguments
+        :returns: object -- Object containing command line options
+    """
+    parser = argparse.ArgumentParser(description="Compute The Hydration Free Energy")
+    parser.add_argument("pdb_input_file", type=str, help="PDB for the ligand to minimize and compute the Hydration Free Energy.")
+    parser.add_argument("-o", "--output_path", type=str, default="./output/", help="Path where to write the results")
+    parser.add_argument("-of", "--output_file", type=str, default="hydration_free_energy.csv", help="File with the energy difference.")
+    args = parser.parse_args()
+    return args.pdb_input_file, args.output_path, args.output_file
+
+def get_outputs():
+	"""
+		It gets the two PDB files from the minimization for the molecule in vacuum and OBC.
+	"""
+	vacuum_input_file = os.path.join(os.getcwd(), 'VACUUM_minimization.out')
+	OBC_input_file = os.path.join(os.getcwd(),'OBC_minimization.out')
+	return vacuum_input_file, OBC_input_file
+
+
+def main(pdb_input_file, out_path, out_file):
+
+    args = parse_args()
+    if out_path != "": out_file = os.path.join(out_path,out_file)
+    #by default, if it need to be a parameter this should be modified
+    PELE_version = '/home/municoy/builds/PELE/PELE-repo_serial/PELE-1.6'
+    #minimize the molecule
+    new_ligand = MM.MoleculeMinimized(pdb_input_file, PELE_version)
+    new_ligand.minimize(pdb_input_file, PELE_version)
+    #computes the Hydration Free Energy
+    vacuum_input_file, OBC_input_file = get_outputs()
+    f = open(os.path.join(os.getcwd(),out_file), 'w')
+    DiffEnergies.compare_energies(f, vacuum_input_file, OBC_input_file)
+    f.close()
+
+if __name__ == "__main__":
+    pdb_input_file, out_path, out_file = parse_args()
+    main(pdb_input_file, out_path, out_file)
