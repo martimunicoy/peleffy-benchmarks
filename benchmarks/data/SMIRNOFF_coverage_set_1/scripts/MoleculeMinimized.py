e91f8bfc57696719a4b8355d4f663a90f3de4d94
Laura Malo <44496034+laumalo@users.noreply.github.com>
2020-08-26 11:53:24 +0200

    Improved script

--- benchmarks/data/SMIRNOFF_coverage_set_1/scripts/MoleculeMinimized.py
+++ benchmarks/data/SMIRNOFF_coverage_set_1/scripts/MoleculeMinimized.py
@@ -1,4 +1,3 @@
-import os 
 
 class MoleculeMinimized:
 	"""
@@ -21,7 +20,8 @@ class MoleculeMinimized:
 		----------
 
 		Load a molecule from a PDB file and minimize it in the vacuum and OBC solvent. 
-       		>>> import MoleculeMinimized as MM
+		
+        >>> import MoleculeMinimized as MM
 
 		>>> new_molecule = MM.MoleculeMinimized('ligand.pdb', '/home/municoy/builds/PELE/PELE-repo_serial/PELE-1.6')
 		>>> new_molecule.minimize(input_file = 'ligand.pdb', PELE_version = '/home/municoy/builds/PELE/PELE-repo_serial/PELE-1.6')
@@ -34,23 +34,69 @@ class MoleculeMinimized:
 		"""
 		It creates an output folder with a copy of the ligand's pdb where all the results will be saved. 
 		"""
-		os.system("mkdir output")
-		os.system("cp %s ./output/" % (input_file))
-		os.system("mv ./output/%s ./output/ligand.pdb" % (input_file))
+		from pathlib import Path
+		import shutil
+		import os 
+
+
+		# Handels path of the input file
+		p = Path(input_file)
+		file, folder = p.name, p.parents[0]
+
+		# It makes the output directory
+		os.makedirs('output',exist_ok = True)
+		shutil.copy(p, os.path.join(os.getcwd(),'output', 'ligand.pdb'))
+
 
 	def _generate_parameters(self):
 		"""
 		It generates the parameters of the molecule (from the input_file) as DataLocal in the output folder.
 		"""
-		os.system("python /home/lauramalo/repos/offpele/offpele/main.py ./output/ligand.pdb --with_solvent --as_DataLocal")
-		os.system("mv ./DataLocal/ ./output/DataLocal/")
+		import offpele
+		from offpele.topology import Molecule
+		from offpele.template import Impact
+		from offpele.solvent import OBC2
+		from offpele.main import handle_output_paths
+		import os 
+
+	
+		# Forcefield and charges method
+		forcefield = 'openff_unconstrained-1.2.0.offxml'
+		charges_method = 'am1bcc'
+
+		# Create representation of a particular molecule
+		PATH_molecule = os.path.join(os.getcwd(),'output', 'ligand.pdb')
+		molecule = Molecule(PATH_molecule)
+
+		# Saving paths
+		rotamer_library_output_path, impact_output_path, solvent_output_path = \
+			handle_output_paths(molecule = molecule, output =os.path.join(os.getcwd(),'output'), as_datalocal = True )
+
+		# Generate its rotamer library
+		rotamer_library = offpele.topology.RotamerLibrary(molecule)
+		rotamer_library.to_file(rotamer_library_output_path)
+
+		# Generate its parameters and template file
+		molecule.parameterize(forcefield, charges_method=charges_method)
+		impact = Impact(molecule)
+		impact.write(impact_output_path)
+
+		# Generate its solvent parameters
+		solvent = OBC2(molecule)
+		solvent.to_json_file(solvent_output_path)
+
 
 	def _link_folders(self):
 		"""
 		It links the encessary folders to the output folder.
 		"""
-		os.system("ln -s /home/municoy/repos/PELE-repo/Data ./output/")
-		os.system("ln -s /home/municoy/repos/PELE-repo/Documents ./output/") 
+		import os 
+
+		PELE_SRC = '/home/municoy/repos/PELE-repo/'
+		
+		os.symlink('{}Data'.format(PELE_SRC), os.path.join(os.getcwd(),'output','Data'))
+		os.symlink('{}Documents'.format(PELE_SRC), os.path.join(os.getcwd(),'output', 'Documents'))
+
 
 	def minimize(self,input_file, PELE_version):
 		"""
@@ -64,9 +110,29 @@ class MoleculeMinimized:
 					  path of an executable version of PELE
 
 		"""
+		import os
+		import requests
+		from pathlib import Path
+
+		VACUUM_CF = '/home/lauramalo/repos/offpele-benchmarks/benchmarks/solvent/Conf/VACUUM_minimization.conf'
+		OBC_CF = '/home/lauramalo/repos/offpele-benchmarks/benchmarks/solvent/Conf/OBC_minimization.conf'
+
 		self._output_folder(input_file)
 		self._link_folders()
 		self._generate_parameters()
+		
+		# Minimization
 		os.chdir("./output/")
-		os.system(" %s /home/lauramalo/tests/geometry/VACUUM_minimization.conf > VACUUM_minimization.out" % (PELE_version))
-		os.system(" %s /home/lauramalo/tests/geometry/OBC_minimization.conf > OBC_minimization.out" % (PELE_version))
+		os.system(" %s %s > VACUUM_minimization.out" % (PELE_version, VACUUM_CF))
+		os.system(" %s %s > OBC_minimization.out" % (PELE_version, OBC_CF))
+
+		# Rename the output folder to the molecule name
+		os.chdir("..")
+		p = Path(input_file)
+		file, folder = p.name, p.parents[0]
+		new_folder = os.path.splitext(file)[0]
+		os.rename('output', new_folder)
+		os.chdir(new_folder)
+
+
+
