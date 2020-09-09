7dadc2b4532a2cb51b15394b41ed5d08e67b9c48
Marti Municoy <martimunicoy@gmail.com>
2020-09-09 13:26:40 +0200

    Update getter.py

--- benchmarks/geometry/getter.py
+++ benchmarks/geometry/getter.py
@@ -41,7 +41,7 @@ class QCPortal(object):
 
         data = dict()
 
-        for name, record in ds.df.records.items():
+        for name, record in ds.data.records.items():
             record_attributes = dict()
             for attribute in attributes:
                 record_attributes[attribute] = record.attributes[attribute]
@@ -49,3 +49,39 @@ class QCPortal(object):
             data[name] = record_attributes
 
         return data
+
+    def get_structures(collection_type, collection_name, output_path):
+        """
+        It writes down the optimized structures from the QCPortal.
+
+        Parameters
+        ----------
+        collection_type : str
+            Type of collection to retrieve
+        collection_name : str
+            Name of the collection to retrieve
+        output_path : str
+            The output path where structures will be saved
+        """
+        import os
+        import qcportal as ptl
+        import json
+
+        os.mkdirs(output_path, exist_ok=True)
+
+        client = ptl.FractalClient()
+        ds = client.get_collection(collection_type, collection_name)
+
+        index = int(1)
+        index_to_name = dict()
+        for name, record in ds.data.records.items():
+            optimization_record = ds.get_record(name, specification='default')
+            opt_molecule = optimization_record.get_final_molecule()
+            opt_molecule.to_file('{}/{}.xyz'.format(output_path, index))
+            index_to_name[index] = name
+            index += int(1)
+
+        json_output = json.dumps(index_to_name)
+
+        with open(os.path.join(output_path, 'index_to_name.json'), "w") as f:
+            f.write(json_output)
