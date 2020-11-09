8f4ed6d517506ec85e4059cc08fc22733ab124d0
Marti Municoy <martimunicoy@gmail.com>
2020-11-09 17:06:12 +0100

    It has to be a keyword argument

--- peleffybenchmarktools/minimization/benchmark.py
+++ peleffybenchmarktools/minimization/benchmark.py
@@ -222,7 +222,7 @@ class MinimizationBenchmark(object):
                 PELE_src=self.PELE_src,
                 PELE_license=self.PELE_license,
                 output_path=output_path,
-                self.forcefield)
+                forcefield=self.forcefield)
             output_file = pele_minimization.run(
                 mol, output_file='PELE_output.txt',
                 pdb_path=distorted_molecule_path)
